2386ba10b892f6dfeb8497f21f98b751410ca61c
sergio15dev <sergio15.dev@gmail.com>
2023-10-07 16:02:32 -0500

    modify InicioPage in react component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Ejemplo from './components/Ejemplo'
+import InicioPage from './pages/InicioPage'
 
 function App() {
 
   return (
     <>
       <div>
-        <Ejemplo />
+        {/* <Ejemplo /> */}
+        <InicioPage />
       </div>
     </>
   )

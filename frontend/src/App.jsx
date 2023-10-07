a8cb3942f816a0b81b844b69677e4cce596331c5
sergio15dev <sergio15.dev@gmail.com>
2023-10-06 21:33:12 -0500

    create base frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Ejemplo from './components/Ejemplo'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Ejemplo />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Ejemplo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Ejemplo() {
+  return (
+    <div>Ejemplo</div>
+  )
+}
+
+export default Ejemplo

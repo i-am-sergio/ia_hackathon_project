9ea6476aae4a79345bddfc088f64e4cbc4c4d41c
sergio15dev <sergio15.dev@gmail.com>
2023-10-07 23:02:38 -0500

    Fix Navigation component

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav className='ml-10'>
+    <nav className='ml-24'>
       <ul className='flex'>
-        <li className='mr-4'>
+        <li className='mr-4 mt-4 px-2 py-2 rounded-sm bg-slate-300'>
           <Link to="/">Login</Link>
         </li>
-        <li className='mr-4'>
+        <li className='mr-4 mt-4 px-2 py-2 rounded-sm bg-slate-300'>
           <Link to="/register">Register</Link>
         </li>
-        <li className='mr-4'>
+        <li className='mr-4 mt-4 px-2 py-2 rounded-sm bg-slate-300'>
           <Link to="/collection">Collection</Link>
         </li>
-        <li>
+        <li className='mr-4 mt-4 px-2 py-2 rounded-sm bg-slate-300'>
           <Link to="/info">InfoPage</Link>
         </li>
       </ul>

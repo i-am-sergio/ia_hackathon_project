0c3aa36f84f14788efa2d99e5608c9e16abf5f63
Lu0811 <luci20huaman@gmail.com>
2023-10-09 22:10:37 -0500

    Login

--- frontend/src/pages/InicioPage.jsx
+++ frontend/src/pages/InicioPage.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
+
 function InicioPage() {
-  function print() {
-    alert("algo aqui"+456);
-  }
 
   return (
     <div className="bg-slate-500 p-8">
       <div id="navbar">
-
       </div>
       <div id="contenido" className="">
         <div className="text-3xl font-bold mb-4">Hello</div>
+
         <img className="w-full mb-4" src="" alt="" />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2"
